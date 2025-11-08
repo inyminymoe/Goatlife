3f175f74869b06e883fca1a201ed8faf6cf1bd31
yoonkyung <kyung3098@naver.com>
2025-11-09 00:04:48 +0900

    feat: board-list page

---
+++ src/app/board/page.tsx
@@ -0,0 +1,9 @@
+import BoardListView from '@/components/features/board/BoardListView';
+
+export default function BoardList() {
+  return (
+    <main className="bg-grey-100 rounded-[5px] px-[25px] py-5 mb-5 col-span-2">
+      <BoardListView />
+    </main>
+  );
+}
