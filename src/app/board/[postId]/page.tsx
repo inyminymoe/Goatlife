e0f7c672ea4be97caa197a27ea560a42d0557921
yoonkyung <kyung3098@naver.com>
2025-11-23 20:54:50 +0900

    feat: 게시판, 댓글 영역 분리

--- src/app/board/[postId]/page.tsx
+++ src/app/board/[postId]/page.tsx
@@ -1,4 +1,5 @@
 import { BoardPostView } from '@/components/features/board-post/BoardPostView';
+import { CommentSection } from '@/components/features/board-post/comment/CommentSection';
 
 export default async function BoardPostPage({
   params,
@@ -6,10 +7,12 @@ export default async function BoardPostPage({
   params: Promise<{ postId: string }>;
 }) {
   const { postId } = await params;
+  // api getPost
 
   return (
-    <main className="bg-grey-100 rounded-[5px] px-[25px] py-5 mb-5 col-span-2 max-w-[1440px]">
+    <main className="col-span-2 max-w-[1440px]">
       <BoardPostView postId={postId} />
+      <CommentSection postId={postId} commentCount={11} />
     </main>
   );
 }
